d493fcb1e3dcd0c8ac243f53768462a9b80391d5
03DAICHI18 <dai.03.chi.18@gmail.com>
2021-02-23 23:29:41 +0900

    ハンバーガーメニュー背景ロック削除

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -29,43 +29,12 @@ jQuery(document).ready(function ($) {
   // *********************
   // ハンバーガーメニュー
   // *********************
-  var scrollValue;
-  var userAgent = window.navigator.userAgent.toLowerCase();
-  var ios = userAgent.indexOf('iphone') > -1 || userAgent.indexOf('ipad') > -1 || userAgent.indexOf('macintosh') > -1 && 'ontouchend' in document;
-
-  function sclollRock() {
-    scrollValue = $(window).scrollTop();
-    if (ios) {
-      $('body').css('position', 'fixed');
-      $('body').css('top', '0');
-    } else {
-      $('body').addClass('is_scrollRock');
-    }
-  }
-  function sclollUnrock() {
-    if (ios) {
-      $('body').css('position', '');
-      $('body').css('top', '-' + scrollValue + 'px');
-      $(window).scrollTop(scrollValue);
-    } else {
-      $('body').removeClass('is_scrollRock');
-    }
-  }
-
   var btn = $('.js_hamburgerBtn_lineTop , .js_hamburgerBtn_lineMiddle  , .js_hamburgerBtn_lineBottom');
   var menu = $('.ly_header');
-
   $('.js_hamburgerBtn').on('click', function () {
     btn.toggleClass('is_open');
     menu.toggleClass('is_open');
-
-    if (btn.hasClass('is_open')) {
-      sclollRock()
-      // $('body').css('padding-right', '17px');
-    } else {
-      sclollUnrock()
-      // $('body').css('padding-right', '0');
-    }
   })
+
 
 });
