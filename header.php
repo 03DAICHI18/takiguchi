1eab8280896227e2e969ad39ee68d9a5e3a16b04
03DAICHI18 <dai.03.chi.18@gmail.com>
2021-03-18 10:07:19 +0900

    header.php OGP部分再コメントアウト

--- header.php
+++ header.php
@@ -10,22 +10,20 @@
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-  <!-- プラグインで設定 -->
-  <!-- <meta name="description" content="福岡でフリーランスの美容師として活動する滝口三奈美のオフィシャルホームページです。ご予約や、メニュー料金のご確認はこちらから宜しくお願いします。" /> -->
+  <meta name="description" content="福岡でフリーランスの美容師として活動する滝口三奈美のオフィシャルホームページです。ご予約や、メニュー料金のご確認はこちらから宜しくお願いします。" />
 
   <!-- Windows用デスクトップ用タイル設定 -->
   <meta name="msapplication-TileImage" content="https://03daichi18.github.io/takiguchi/public/assets/img/human/human01.jpg" />
   <meta name="ç" content="#afc9c9" />
 
-  <!-- SNS共通 -->
-  <meta property="og:url" content="" /> <!-- ページURL -->
+  <!-- SNS共通 OGPプラグインで設定-->
+  <!-- <meta property="og:url" content="" />
   <meta property="og:title" content="滝口三奈美 official HP" />
   <meta property="og:image" content="https://03daichi18.github.io/takiguchi/public/assets/img/human/human01.jpg" />
   <meta property="og:site_name" content="滝口三奈美 official HP" />
   <meta property="og:type" content="website" />
   <meta property="og:description" content="福岡でフリーランスの美容師として活動する滝口三奈美のオフィシャルホームページです。ご予約や、メニュー料金のご確認はこちらから宜しくお願いします。" />
-  <meta property="og:locale" content="ja_JP" />
+  <meta property="og:locale" content="ja_JP" /> -->
 
   <!-- Twitter OGPプラグインで設定 -->
   <!-- <meta name="twitter:card" content="summary_large_image" />
